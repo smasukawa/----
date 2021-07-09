9855d2497cf95daef5a28d71944875863f0ffbab
syuQ <smasukawa@hotmail.com>
2021-07-09 09:17:46 +0900

    1st

--- 行数集計.bat
+++ 行数集計.bat
@@ -1,2 +1,13 @@
-powershell .\件数集計.ps1
+@echo off
+powershell .\行数集計.ps1
+if %ERRORLEVEL% NEQ 0 goto FAILURE
+goto END
+
+:FAILURE
+echo ERROR STOP: %ERRORLEVEL%
 pause
+
+:END
+echo 行数集計が完了しました
+pause
+exit
